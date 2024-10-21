01cc2400be6ec91986abaa65a54892556d14cbdd
Rob Wistrand <robwistrand@gmail.com>
2024-10-21 10:59:45 -0700

    First

---
+++ app.js
@@ -0,0 +1,13 @@
+// we want each button to respond to to a click.
+// we want each button to print to div class display
+// we want to define operations and result of operations
+
+/*-------------------------------- Constants --------------------------------*/
+
+/*-------------------------------- Variables --------------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
